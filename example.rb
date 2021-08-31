1927a284d548eb3644ef95fd81f9fdab214fc9b8
Nathan Woodhull <woodhull@gmail.com>
2021-08-31 14:10:23 -0400

    modernize syntax.

--- example.rb
+++ example.rb
@@ -1,7 +1,7 @@
 require 'oauth2'
 
 # START CONFIGURATION SETTINGS
-# Replace these variable values with values for your instance of your platform. 
+# Replace these variable values with values for your instance of your platform.
 client_id = 'xxabc123'
 client_secret = 'xxabc123'
 # Replace this with your platform URL
@@ -11,7 +11,7 @@ constrolshift_url = 'https://demo.controlshiftlabs.com'
 client = OAuth2::Client.new(client_id, client_secret, site: constrolshift_url)
 
 token = client.client_credentials.get_token
-response = token.get("/api/v1/members/lookup?email=woodhull@gmail.com")
+token.get('/api/v1/members/lookup', params: {'email' => 'woodhull@gmail.com'})
 
-# This will print the response. You likely want to do something else useful with this information. 
+# This will print the response. You likely want to do something else useful with this information.
 puts json = JSON.parse(response.body)
